35b181dc72d9f37f0a6084ca9eb02c11c8272c18
22550337 <124700514+22550337@users.noreply.github.com>
2023-02-07 08:31:54 -0700

    Add files via upload

---
+++ Practica/Hola.java
@@ -0,0 +1,5 @@
+public class Holi{
+  public static void main (String[] arg){
+System.out.printIn("Hola mundo"); 
+   }
+}
---
+++ Practica/Holi.java
@@ -0,0 +1,8 @@
+public class Eva1_1HOLAMUNDO {
+
+    
+    public static void main(String[] args) {
+        System.out.print("Hola mundo");
+    }
+    
+}
